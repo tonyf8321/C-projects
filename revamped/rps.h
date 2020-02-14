f3c7cc1c1e683340a9fe3353b9ea2b9660bcadd6
Tony Fountain <tonyf8321@gmail.com>
2020-02-14 11:12:37 -0600

    removed finish date in author section

--- revamped/rps.h
+++ revamped/rps.h
@@ -4,7 +4,6 @@ Author:      Tony Fountain
 Purpose:     Created because I wanted to challenge myself, learn, and 
              expand knowledge.
 start date:  12/31/2019
-finish date: 01/05/2020
 Description of program: It allows the user to input a name to be used as 
 the account, play 1v1, play vs 2 others so 1v1v1, and a ranked system 
 using 1v1 with a simple ranking score gain equation in place.
